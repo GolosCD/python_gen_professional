82ac1e7be0202c19d34efe59f6f191a068a788df
GolosCD <golosand@gmail.com>
2023-12-03 11:19:27 +0300

    Итератор DictItemsIterator

---
+++ 10.4.6.py
@@ -0,0 +1,67 @@
+class DictItemsIterator():
+
+    def __init__(self,my_dict :dict):
+        self.my_dict = my_dict
+        self.k = iter(my_dict)
+
+
+    def __iter__(self):
+        return self
+        
+    
+    def __next__(self):
+        k = next(self.k)
+        return (k,self.my_dict.get(k))
+        
+
+
+# INPUT DATA:
+
+# TEST_1:
+pairs = DictItemsIterator({1: 'A', 2: 'B', 3: 'C'})
+
+print(*pairs)
+
+# TEST_2:
+data = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49}
+
+pairs = DictItemsIterator(data)
+
+print(*pairs)
+
+# TEST_3:
+data = {'Arthur': 100, 'Timur': 100, 'Dima': 100,
+        'Anri': 101, 'Roma': 99, 'German': 98}
+
+pairs = DictItemsIterator(data)
+
+print(list(pairs))
+
+# TEST_4:
+data = {'Arthur': [100, 5], 'Timur': [100, 6], 'Dima': [100, 7, 8], 
+        'Anri': [100, 101], 'Roma': [99]}
+
+pairs = DictItemsIterator(data)
+
+print(next(pairs))
+print(next(pairs))
+print(next(pairs))
+print(next(pairs))
+print(next(pairs))
+
+try:
+    print(next(pairs))
+except StopIteration:
+    print('Error')
+
+# TEST_5:
+data = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7}
+
+pairs = DictItemsIterator(data)
+
+print(tuple(pairs))
+
+try:
+    print(next(pairs))
+except StopIteration:
+    print('Error')

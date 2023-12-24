5f695dd4c26b316847f9971418b151cc0c3d9d18
AnanevSYU <ananevsyu@vip.cbr.ru>
2023-12-24 10:38:11 +0300

    Функция ranges() 🌶️

--- 0_file_for_test.py
+++ 0_file_for_test.py
@@ -1,3 +1,31 @@
-a = 'Елькина'
+numbers = [1, 2, 3, 4, 7, 8, 10]
 
-print(sorted(a)[0])
+for index , val in enumerate(numbers,start=0):
+    curent_val = val
+    curent_index = index
+    try:
+        next_val = numbers[curent_index+1]
+        last_val = numbers[curent_index-1]
+    except:
+        next_val = curent_val
+        last_val = curent_val
+        
+    # print('=================================')    
+    # print(f'curent_val:= {curent_val}')
+    # print(f'curent_index:= {curent_index}')
+    # print(f'next_val:= {next_val}')
+    # print(f'last_val:= {last_val}')
+    # print('=================================')    
+        
+    if next_val-curent_val==1:
+        
+        print(f'({curent_val}:{next_val})')
+    elif next_val-curent_val!=1 and curent_val-last_val==1:
+        continue
+    else:
+        print(f'({curent_val}:{curent_val})')
+        
+    # print(f'{index} : {val}')
+    
+    
+print(len(numbers),numbers.index(10))    
